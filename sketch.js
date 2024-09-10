70ba8697717b7e530fccb181cc97b2583e3b3389
ZOE TALAMANTEZ <zt23f@fsu.edu>
2024-09-10 18:32:37 -0400

    Added a second shape, changed size, and color

--- sketch.js
+++ sketch.js
@@ -1,28 +1,49 @@
-let x = 100;     // x position of the ball
-let y = 0;       // y position of the ball
-let xSpeed = 3;  // speed in X direction
-let ySpeed = 7; // speed in Y direction
-let d = 20;      // diameter of the ball
+let x1 = 100;     // x position of the ball
+let y1 = 0;       // y position of the ball
+let xSpeed1 = 3;  // speed in X direction
+let ySpeed1 = 7; // speed in Y direction
+let d1 = 20;      // diameter of the ball
+
+let x2 = 10;     // x position of the ball
+let y2 = 0;       // y position of the ball
+let xSpeed2 = 3;  // speed in X direction
+let ySpeed2 = 7; // speed in Y direction
+let d2 = 50;      // diameter of the ball
 
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220); // try commenting this out!
+  background(51); // try commenting this out!
  
   // if x hits the right side or the left side of the canvas,
   // switch directions...
-  if (x > width || x < 0) {
-    xSpeed = xSpeed * -1;  
+  if (x1 > width || x1 < 0) {
+    xSpeed1 = xSpeed1 * -1;  
   }
-  x = x + xSpeed; // adding a negative xSpeed *decreases* x, right?
+  x1 = x1 + xSpeed1; // adding a negative xSpeed *decreases* x, right?
   
-  // same as above, but for the top and bottom of the canvas
-  if (y > width || y < 0) {
-    ySpeed = ySpeed * -1;  
+  if (x2 > width || x2 < 0) {
+    xSpeed2 = xSpeed2 * -1;  
   }
-  y = y + ySpeed;
+  x2 = x2 + xSpeed2;
 
-  ellipse(x, y, d);
+  // same as above, but for the top and bottom of the canvas
+  if (y1 > width || y1 < 0) {
+    ySpeed1 = ySpeed1 * -1;  
+  }
+  y1 = y1 + ySpeed1;
+
+  if (y2 > width || y2 < 0) {
+    ySpeed2 = ySpeed2 * -1;  
+  }
+  y2 = y2 + ySpeed2;
+
+
+  fill(252, 3, 211)
+  ellipse(x1, y1, d1);
+
+  fill(3, 252, 215)
+  ellipse(x2, y2, d2);
 }
